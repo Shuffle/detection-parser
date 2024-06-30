006021a58cd6055cc45574137795786ad943835e
Aditya <60684641+0x0elliot@users.noreply.github.com>
2024-06-30 17:33:40 +0530

    docs: Name changes

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='shuffle-email-rules',
-    version='0.8',
+    name='shuffle-heimdall',
+    version='0.1',
     description='An open-source language for email filtering rules.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Aditya',
     package_data=
     {
-        'shuffle_email_rules': ['lib/binaries/*']
+        'heimdall': ['lib/binaries/*']
     },
     author_email='aditya@shuffler.io', # pls don't spam me
-    packages=['shuffle_email_rules'],
+    packages=['heimdall'],
     # packages=find_packages(),
     install_requires=[],  # Add any dependencies here
 )
--- shuffle_email_rules/__init__.py
+++
@@ -1,5 +0,0 @@
-# shuffle-email-rules/__init__.py
-
-from .evaluate import evaluate_email_expression
-
-__all__ = ['evaluate_email_expression']
